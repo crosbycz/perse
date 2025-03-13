8df950586e7c2449dd55c50fd6a820fdfbe98953
Netext <daniel.dimun@outlook.cz>
2025-03-13 05:47:09 +0100

    New update of metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,10 +11,47 @@ import Footer from "@/components/footer"
 const inter = Inter({ subsets: ["latin"] })
 
 export const metadata: Metadata = {
-  title: "ReeCom - Stack bands, not scams!",
-  description: "Best ReeCom shop",
-    generator: 'v0.dev'
-}
+  title: "Perse - Stack bands, not scams!",
+  description:
+    "Persa is an online store where you can find over several bank accounts that contain several thousand deals thanks to the methods we can afford to sell. We have extremely low market prices and quality workmanship.",
+  generator: "persa",
+  keywords: [
+    "persa",
+    "stack bands",
+    "stack",
+    "bands",
+    "online store",
+    "bank accounts",
+    "deals",
+    "methods",
+    "market prices",
+    "quality workmanship",
+  ],
+  openGraph: {
+    title: "Perse - Stack bands, not scams!",
+    description:
+      "Persa is an online store where you can find over several bank accounts that contain several thousand deals thanks to the methods we can afford to sell. We have extremely low market prices and quality workmanship.",
+    images: [
+      {
+        url: "/Shop.png",
+        width: 1200,
+        height: 630,
+        alt: "Shop banner",
+      },
+    ],
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Perse - Stack bands, not scams!",
+    description:
+      "Persa is an online store where you can find over several bank accounts that contain several thousand deals thanks to the methods we can afford to sell. We have extremely low market prices and quality workmanship.",
+    images: ["/Shop.png"],
+  },
+  icons: {
+    icon: "/Shop.png",
+  },
+};
 
 export default function RootLayout({
   children,
